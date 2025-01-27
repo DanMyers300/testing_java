25c08f0a599e60c31675af341808db30f932c386
dan <contact@danmyers.net>
2025-01-27 15:12:47 -0600

    added test texture

--- src/main/java/net/danmyers/testing_java/ModItems.java
+++ src/main/java/net/danmyers/testing_java/ModItems.java
@@ -6,6 +6,9 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+
+import net.fabricmc.fabric.api.registry.*;
+
 import java.util.List;
 import java.util.Locale;
 import java.util.HashMap;
@@ -17,27 +20,45 @@ public class ModItems {
         return registeredItem;
     }
 
+    public static Item getItem(RegistryKey<Item> key) {
+        Item item = ModItems.register(
+            new Item(new Item.Settings().registryKey(key)),
+            key
+        );
+        return item;
+    };
+
+    public static RegistryKey<Item> generateKey(String name) {
+        Identifier id = Identifier.of(TestingJava.MOD_ID, name);
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+        return key;
+    };
+
     public static void handleRegister(
         List<String> itemList,
         Map<String,RegistryKey<Item>> keyMap,
         Map<String, Item> valueMap)
     {
         for (String name : itemList) {
-            // Generate registry key
             String keyName = name.toUpperCase(Locale.ROOT) + "_KEY";
-            Identifier id = Identifier.of(TestingJava.MOD_ID, name);
-            RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
-            keyMap.put(keyName, key);
+            keyMap.put(keyName, generateKey(name));
             
-            Item item = ModItems.register(
-                new Item(new Item.Settings().registryKey(key)),
-                key
-            );
+            Item item = getItem(generateKey(name));
             valueMap.put(name, item);
         }
-    }
+    };
+
+    public static void makeFuel(String itemName, int burnTime) {
+        // Add the suspicious substance to the registry of fuels, with a burn time of 30 seconds.
+        // Remember, Minecraft deals with logical based-time using ticks.
+        // 20 ticks = 1 second.
+        FuelRegistryEvents.BUILD.register((builder, context) -> {
+        	builder.add(Ingredients.INGREDIENT_ITEMS.get(itemName), burnTime * 20);
+        });
+    };
 
     public static void initialize() {
         Ingredients.initialize();
+        makeFuel("suspicious_substance", 30);
     }
 }
